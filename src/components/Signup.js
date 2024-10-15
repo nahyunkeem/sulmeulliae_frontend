4d7adf9c3e47ef678bd6947c1b72fbeb4668a54a
gr22nsky <gr2nsky@gmail.com>
2024-10-15 22:51:52 +0900

    signup_url_edit

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -25,7 +25,7 @@ function Signup() {
         e.preventDefault();
 
         // 회원가입 API 호출
-        api.post('/accounts/', formData)
+        api.post('/accounts/signup/', formData)
             .then(() => {
                 alert('회원가입이 완료되었습니다. 로그인 페이지로 이동합니다.');
                 navigate('/login');

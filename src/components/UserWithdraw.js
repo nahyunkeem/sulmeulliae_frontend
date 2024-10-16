591110b76b121055630a45ae77aa3080d1e6b005
gr22nsky <gr2nsky@gmail.com>
2024-10-16 20:28:13 +0900

    code_fix

--- src/components/UserWithdraw.js
+++ src/components/UserWithdraw.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import api from '../services.api';
+import api from '../services/api';
 import { useNavigate } from 'react-router-dom';
 
 function UserDelete() {

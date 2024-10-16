84aeec82459699f0fb8884888dc226bc569584dd
gr22nsky <gr2nsky@gmail.com>
2024-10-16 11:18:27 +0900

    default_image

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -23,9 +23,7 @@ function UserProfile() {
     return (
         <div>
             <h2>회원 정보</h2>
-            <img 
-                src={`https://api.sulmeulliae.com${userProfile.profile_image}` || 'https://api.sulmeulliae.com/static/default-profile.jpeg'}  // profile_image가 없으면 기본 이미지 사용
-                alt="프로필 이미지" 
+            <img src={userProfile.profile_image ? `https://api.sulmeulliae.com${userProfile.profile_image}` : "{% static 'default-profile.jpeg' %}"} alt="프로필 이미지"
                 style={{ width: '150px', height: '150px', borderRadius: '50%' }} // 프로필 이미지 스타일 설정
             />
             <p>이름: {userProfile.fullname}</p>

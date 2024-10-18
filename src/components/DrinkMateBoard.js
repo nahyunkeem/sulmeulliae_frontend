273f46fa92953625451380c7c5fcc9468da1c2e0
gr22nsky <gr2nsky@gmail.com>
2024-10-18 12:29:16 +0900

    bug

--- src/components/DrinkMateBoard.js
+++ src/components/DrinkMateBoard.js
@@ -80,7 +80,7 @@ function DrinkMateBoard() {
                         </li>
                     ))
                 ) : (
-                    <p>로그인하셨다면 새로고침해주세요..</p>
+                    <p>채팅방을 생성해주세요..</p>
                 )}
             </ul>
         </div>
